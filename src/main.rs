2a86dfcaa257de40f43df8b6bfb68e58baf86eec
kyunghyunHan <ekfqlwcjswl@naver.com>
2025-03-13 22:15:21 +0900

    feat:test

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let provider = yahoo::YahooConnector::new().unwrap();
 
     // 날짜 범위를 2년으로 확장
-    let start_date = Date::from_calendar_date(2024, Month::January, 1)
+    let start_date = Date::from_calendar_date(2000, Month::January, 1)
         .unwrap()
         .with_hms(0, 0, 0)
         .unwrap()
@@ -21,20 +21,20 @@ fn main() {
     // SCHD 배당금 정보
     println!("SCHD 배당금 정보:");
     let response =
-        tokio_test::block_on(provider.get_quote_history("QDTE", start_date, end_date)).unwrap();
+        tokio_test::block_on(provider.get_quote_history("SPY", start_date, end_date)).unwrap();
     let dividends = response.dividends().unwrap();
     for dividend in dividends {
         let timestamp = dividend.date;
         let datetime = OffsetDateTime::from_unix_timestamp(timestamp as i64).unwrap();
         println!(
-            "배당금: ${:.4}, 날짜: {}-{:02}-{:02}", 
-            dividend.amount, 
-            datetime.year(), 
-            u8::from(datetime.month()), 
+            "배당금: ${:.4}, 날짜: {}-{:02}-{:02}",
+            dividend.amount,
+            datetime.year(),
+            u8::from(datetime.month()),
             datetime.day()
         );
     }
-    
+
     // 다른 ETF와 비교 (예: SPY)
     println!("\nSPY 배당금 정보:");
     let spy_response =
@@ -44,10 +44,10 @@ fn main() {
         let timestamp = dividend.date;
         let datetime = OffsetDateTime::from_unix_timestamp(timestamp as i64).unwrap();
         println!(
-            "배당금: ${:.4}, 날짜: {}-{:02}-{:02}", 
-            dividend.amount, 
-            datetime.year(), 
-            u8::from(datetime.month()), 
+            "배당금: ${:.4}, 날짜: {}-{:02}-{:02}",
+            dividend.amount,
+            datetime.year(),
+            u8::from(datetime.month()),
             datetime.day()
         );
     }
